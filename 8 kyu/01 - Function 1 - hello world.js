93f57fa12f377a05cacb6f6d7a331ea9cb09b177
Evgeny <evgenxvi@proton.me>
2025-02-14 11:23:32 +0500

    Add solution "Function 1 - hello world"

---
+++ 8 kyu/01 - Function 1 - hello world.js
@@ -0,0 +1,9 @@
+// Function 1 - hello world
+// https://www.codewars.com/kata/function-1-hello-world
+
+// Solution:
+// Solution with explicit return and variable declaration.
+function greet() {
+  const greetMessage = "hello world!";
+  return greetMessage;
+};
