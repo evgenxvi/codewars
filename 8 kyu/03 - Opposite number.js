549b66cd176e6ad80098e3aff278ccf38ad49eea
Evgeny <evgenxvi@proton.me>
2025-02-14 11:29:57 +0500

    Add solution "Opposite number"

---
+++ 8 kyu/03 - Opposite number.js
@@ -0,0 +1,11 @@
+// Opposite number
+// https://www.codewars.com/kata/opposite-number
+
+// Solution:
+// Returns the negation of "number".
+function opposite(number) {
+  return -number;
+};
+
+// This subtracts the number from zero to get the same result.
+const opposite = (number) => 0 - number;
