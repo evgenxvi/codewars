92bb16e8b7345e108355f6e1b64ee5ebd9b481a7
Evgeny <evgenxvi@proton.me>
2025-02-17 08:34:16 +0500

    Add solution "Convert a String to a Number!"

---
+++ 8 kyu/07 - Convert a String to a Number!.js
@@ -0,0 +1,8 @@
+// Convert a String to a Number!
+// https://www.codewars.com/kata/convert-a-string-to-a-number
+
+// Solution:
+// Simple solution using "Number()" constructor.
+const stringToNumber = function(str) {
+  return Number(str);
+};
