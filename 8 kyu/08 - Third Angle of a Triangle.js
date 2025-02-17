e8c88b3247dd6922ef63ff42bb22e9925a394a79
Evgeny <evgenxvi@proton.me>
2025-02-17 08:39:41 +0500

    Add solution "Third Angle of a Triangle"

---
+++ 8 kyu/08 - Third Angle of a Triangle.js
@@ -0,0 +1,8 @@
+// Third Angle of a Triangle
+// https://www.codewars.com/kata/third-angle-of-a-triangle
+
+// Solution:
+// Subtracts the sum of the two given angles from 180.
+function otherAngle(a, b) {
+  return 180 - (a + b);
+};
