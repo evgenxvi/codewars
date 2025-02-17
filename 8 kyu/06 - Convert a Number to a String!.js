4f8d3e2e100e0376cb910900dbce2a2878cee841
Evgeny <evgenxvi@proton.me>
2025-02-17 08:28:56 +0500

    Add solution "Convert a Number to a String!"

---
+++ 8 kyu/06 - Convert a Number to a String!.js
@@ -0,0 +1,8 @@
+// Convert a Number to a String!
+// https://www.codewars.com/kata/convert-a-number-to-a-string
+
+// Solution:
+// Simple solution using "String()" constructor.
+function numberToString(num) {
+  return String(num);
+};
